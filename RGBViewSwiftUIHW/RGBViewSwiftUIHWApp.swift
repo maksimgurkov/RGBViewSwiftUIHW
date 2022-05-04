3cc57157f4ebf2337040e1392a940d6bda4752d5
Maksim Gurkov <maksim.gurkov@gmail.com>
2022-05-04 16:56:29 +0300

    Initial Commit

---
+++ RGBViewSwiftUIHW/RGBViewSwiftUIHWApp.swift
@@ -0,0 +1,17 @@
+//
+//  RGBViewSwiftUIHWApp.swift
+//  RGBViewSwiftUIHW
+//
+//  Created by Максим Гурков on 04.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct RGBViewSwiftUIHWApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
